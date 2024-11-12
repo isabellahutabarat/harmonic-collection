77636048973854e7f563b7b5381358b90e19cc71
isabellahutabarat <hutai493@newschool.edu>
2024-11-12 16:13:15 -0500

    entry-6

---
+++ entry-6/<script>.js
@@ -0,0 +1,8 @@
+<script>
+  document.addEventListener("wheel", function (event) {
+    if (event.deltaY !== 0) {
+      event.preventDefault();
+      document.querySelector(".horizontal-scroll").scrollLeft += event.deltaY;
+    }
+  });
+</script> 
