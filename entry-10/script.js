84599976e5dcd9da0c71ad81685b596a30932f88
isabellahutabarat <hutai493@newschool.edu>
2024-12-03 17:54:58 -0500

    edited header and new entries

---
+++ entry-10/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("wheel", function (event) {
+    if (event.deltaY !== 0) {
+      event.preventDefault();
+      document.querySelector(".horizontal-scroll").scrollLeft += event.deltaY;
+    }
+  });
+
+  
