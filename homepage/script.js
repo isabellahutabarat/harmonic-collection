782f32266150a568ad768abc827f4ab4c8dc1dee
isabellahutabarat <hutai493@newschool.edu>
2024-12-09 03:06:15 -0500

    homepage

--- homepage/script.js
+++ homepage/script.js
@@ -11,3 +11,16 @@ let hideDropdown = () => {
 
 spine.addEventListener("mouseenter", extendDropdown);
 spine.addEventListener("mouseleave", hideDropdown)
+
+let leftSpine = document.querySelector('#left-spine');
+let spineContent = document.querySelector('.spine--content');
+
+leftSpine.addEventListener('mouseenter', () => {
+    spineContent.style.visibility = 'visible';
+    spineContent.style.opacity = '1';
+});
+
+leftSpine.addEventListener('mouseleave', () => {
+    spineContent.style.visibility = 'hidden';
+    spineContent.style.opacity = '0';
+});
